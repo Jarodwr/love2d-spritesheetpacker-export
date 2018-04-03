4c06c2500ea185833625b5635cdf114a852fe5a6
Jarodwr <wrjarod@gmail.com>
2018-04-03 22:30:10 +1000

    source size fix
    actually proper works now

--- lua_export.js
+++ lua_export.js
@@ -1,9 +1,9 @@
-function exportSpriteSheet(dataFilePath, imageFilePath, spriteFrames) {
+function exportSpriteSheet(dataFilePath, imageFilePath, spriteFrames, textureSize) {
     var lua_table = 'return {\n'
     for (var key in spriteFrames) {
         var frame = spriteFrames[key].frame
         var sourceSize = spriteFrames[key].sourceSize
-        lua_table += '\t["' + key + '"] = love.graphics.newQuad(' + frame.x + ', ' + frame.y + ', ' + frame.width + ', ' + frame.height + ', ' + sourceSize.width + ', ' + sourceSize.height + '),\n'
+        lua_table += '\t["' + key + '"] = love.graphics.newQuad(' + frame.x + ', ' + frame.y + ', ' + frame.width + ', ' + frame.height + ', ' + textureSize.width + ', ' + textureSize.height + '),\n'
     }
     lua_table += "}"
 
